151bc3006ce459aacd49aceeaf1f1a0ba2c59c98
eslamabdelbasset1 <eslamabdelbasset1@gmail.com>
2023-12-09 02:08:45 +0200

    create send email to users with queue

---
+++ app/Http/Controllers/sendEmailController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Jobs\SendMailJob;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class sendEmailController extends Controller
+{
+    public function form()
+    {
+        return view('send_emails_form');
+    }
+
+    public function send_emails(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'title' => 'required',
+            'body' => 'required',
+        ]);
+
+        if ($validator->fails()) {
+            return back()->withErrors($validator)->withInput();
+        }
+
+        $details = [
+            'title' => $request->title,
+            'body' => $request->body,
+        ];
+
+        $job = (new SendMailJob($details));
+        dispatch($job);
+
+        return back()->with('status', 'Mails sent successfully');
+
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\sendEmailController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::get('/send_emails', [sendEmailController::class, 'form'])->name('send_emails_form');
+Route::post('/send_emails', [sendEmailController::class, 'send_emails'])->name('send_emails');
+
+
